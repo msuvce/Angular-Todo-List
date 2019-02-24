c3c418bb2e696ea98e6cc05494714f52c30bc95e
Paulo Cunha <paulo_cunha1997@outlook.pt>
2019-02-24 21:00:08 -0100

    Add toggle for TODO completion and delete methods

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -26,4 +26,12 @@ export class TodoItemComponent implements OnInit {
     return classes;
   }
 
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('Delete');
+  }
+
 }
