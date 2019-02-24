05ec072744699dc8ec0e404b552fbe8828eb9aff
Paulo Cunha <paulo_cunha1997@outlook.pt>
2019-02-24 21:26:10 -0100

    Fetching TODO's from an API (JSON placeholder) - data not hardcoded

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,30 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: true
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);  
   }
 
 }
